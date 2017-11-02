08482f72401847cad1cb80ccce610a485bd23ad9
Howard Yeh <howard@metacircus.com>
2017-11-02 22:30:27 +0800

    export everything from sub module

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-export {
-  IContractInfo,
-  IContractCallDecodedResult,
-  Contract,
-} from "./Contract"
-
-export {
-  IRPCCallContractResult,
-  IRPCSendToContractResult,
-  QtumRPC,
-} from "./QtumRPC"
-
-export { IABIMethod } from "./ethjs-abi"
+export * from "./Contract"
+export * from "./QtumRPC"
+export * from "./ContractSendReceipt"
+export * from "./ethjs-abi"
 export { encodeInputs, decodeOutputs } from "./abi"

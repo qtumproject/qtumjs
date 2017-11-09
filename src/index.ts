f2c16a14005488246da6228c0391559735daeae6
Howard Yeh <howard@metacircus.com>
2017-11-09 16:26:31 +0800

    Buffer polyfill for browser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+// Browser polyfill required by ethjs-abi
+// https://github.com/ethjs/ethjs-abi/blob/5e2d4c3b7207111c143ca30d01d743c28cfb52f6/src/utils/index.js#L28
+if (typeof Buffer === "undefined") {
+  const { Buffer } = require("buffer")
+  Object.assign(window, {
+    Buffer,
+  })
+}
+
 export * from "./abi"
 export * from "./Contract"
 export * from "./QtumRPC"

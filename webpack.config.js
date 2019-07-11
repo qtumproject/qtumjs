31b9cddb2f7a376950dcb336ab307e329c874faa
Howard Yeh <hayeah@gmail.com>
2019-07-11 16:20:52 +0800

    fix webpack4 build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-const path = require('path')
+const path = require("path");
+
+const mode = process.env.NODE_ENV || "production";
 
 module.exports = {
-	entry: './src/index.ts',
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	output: {
-		filename: 'qtum.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: "Qtum",
-		libraryTarget: "umd",
-	}
-}
+  mode,
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "qtum.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "Qtum",
+    libraryTarget: "umd"
+  }
+};
